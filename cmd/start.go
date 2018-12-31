b1f7a6716afc2b6f3b83164df64ca5713bd3fe2b
Mya Pitzeruse <j.mya.pitz@gmail.com>
2018-12-31 11:38:11 -0600

    gh-13: moved command into cmd folder and added use on cobra for command management

---
+++ cmd/start.go
@@ -0,0 +1,110 @@
+package cmd
+
+import (
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+	"flag"
+	"github.com/mjpitz/gitfs/pkg/clone"
+	"github.com/mjpitz/gitfs/pkg/config"
+	"github.com/mjpitz/gitfs/pkg/filesystem"
+	"github.com/mjpitz/gitfs/pkg/remotes"
+	"github.com/mjpitz/gitfs/pkg/tree"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"os"
+	"os/user"
+	"path"
+	"strconv"
+	"strings"
+)
+
+var StartCommand = &cobra.Command{
+	Use:   "start [configfile]",
+	Short: "Short description shown in 'help' output",
+	Long:  "Long message show in the 'helo command' output",
+	Run: func(cmd *cobra.Command, args []string) {
+		current, err := user.Current()
+		if err != nil {
+			fail("failed to determine current user")
+		}
+
+		props := path.Join(current.HomeDir, ".gitfs/config.yml")
+		flag.StringVar(&props, "config", props, "Specify the configuration path")
+		flag.Parse()
+
+		if len(props) == 0 {
+			fail("missing config file")
+		}
+
+		cfg, err := config.Load(os.ExpandEnv(props))
+		if err != nil {
+			fail("failed to parse configuration: %v", err)
+		}
+
+		remote, err := remotes.ParseConfig(cfg)
+		if err != nil {
+			fail("failed to parse remote configuration: %v", err)
+		}
+
+		mountpoint := os.ExpandEnv(cfg.Mount)
+		logrus.Infof("[main] configured mount point: %s", mountpoint)
+
+		tree := gitfstree.NewTreeNode()
+
+		logrus.Info("[main] fetching repositories")
+		repositories, err := remote.ListRepositories()
+		if err != nil {
+			fail("failed to fetch repositories: %v", err)
+		}
+
+		logrus.Info("[main] parsing repositories into a directory structure")
+		for _, repository := range repositories {
+			start := 4
+			separator := strings.Index(repository, ":")
+			end := len(repository) - 4
+
+			// git@code.corp.indeed.com:squall/yellowhat-snapshots-prod.git
+			// git@<<HOST>>:<<PATH>>.git
+			host := repository[start:separator]
+			path := repository[separator+1 : end]
+
+			// expand into directory structure
+			parts := strings.Split(path, "/")
+
+			var fullPathParts []string
+			fullPathParts = append(fullPathParts, host)
+			fullPathParts = append(fullPathParts, parts...)
+
+			tree.Insert(fullPathParts, repository)
+		}
+
+		logrus.Infof("[main] attempting to mount %s", mountpoint)
+		c, err := fuse.Mount(mountpoint)
+		if err != nil {
+			fail("failed to mount mountpoint: %v", err)
+		}
+		defer c.Close()
+
+		uid, _ := strconv.Atoi(current.Uid)
+		gid, _ := strconv.Atoi(current.Gid)
+
+		cloner := clone.NewCloner(cfg.Clone)
+
+		filesys := &filesystem.FileSystem{
+			Uid:    uint32(uid),
+			Gid:    uint32(gid),
+			Tree:   tree,
+			Cloner: cloner,
+		}
+
+		logrus.Info("[main] now serving file system")
+		if err := fs.Serve(c, filesys); err != nil {
+			fail("failed to serve filesystem: %v", err)
+		}
+
+		<-c.Ready
+		if err := c.MountError; err != nil {
+			fail("file system failed to start: %v", err)
+		}
+	},
+}
--- go.mod
+++ go.mod
@@ -2,18 +2,26 @@ module github.com/mjpitz/gitfs
 
 require (
 	bazil.org/fuse v0.0.0-20180421153158-65cc252bf669
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/ghodss/yaml v1.0.0
 	github.com/gogo/protobuf v1.2.0
 	github.com/golang/protobuf v1.2.0
 	github.com/google/go-github/v20 v20.0.0
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/nytlabs/gojee v0.0.0-20140331152115-5a79a1542dc0
 	github.com/pkg/errors v0.8.0
 	github.com/sirupsen/logrus v1.2.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
 	github.com/stretchr/testify v1.2.2
-	golang.org/x/net v0.0.0-20181129055619-fae4c4e3ad76
-	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
-	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
+	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9 // indirect
+	golang.org/x/net v0.0.0-20181220203305-927f97764cc3
+	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
+	golang.org/x/sys v0.0.0-20181228144115-9a3f9b0469bb // indirect
+	google.golang.org/appengine v1.4.0 // indirect
 	gopkg.in/src-d/go-billy.v4 v4.3.0
+	gopkg.in/src-d/go-git-fixtures.v3 v3.3.0 // indirect
 	gopkg.in/src-d/go-git.v4 v4.8.1
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
